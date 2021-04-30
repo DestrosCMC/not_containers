93c94713925ff9c909dd82f431adc4dba79d689e
Kieran Daly <kdaly23@cmc.edu>
2021-04-30 18:16:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -9,20 +9,20 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
- name="csci-046-data-structures",
- version="1.0.0",
- description="Implementing Multiple Data Structures",
- long_description=README,
- url="https://github.com/DestrosCMC/not_containers/tree/master",
- author="DestrosCMC",
- author_email="kdaly23@cmc.edu",
- license="MIT",
- classifiers=[
-     "License :: OSI Approved :: MIT License",
-     "Programming Language :: Python :: 3",
-     "Programming Language :: Python :: 3.7",
-             ],
- packages=["containers"],
- include_package_data=True,
- long_description_content_type='text/markdown',
- install_requires=["pytest", "attrs", "hypothesis"])
+    name="csci046-data-structures",
+    version="1.0.0",
+    description="Data structures from CSCI046",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DestrosCMC/not_containers/tree/master",
+    author="DestrosCMC",
+    author_email="kdaly23@cmc.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests")),
+    include_package_data=True,
+)
